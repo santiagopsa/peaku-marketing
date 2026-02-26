b0abdacf9e0e06f92696b6a6dd3159a1c2874504
Santiagopsa <santiagopsa@gmail.com>
2026-02-26 17:24:12 -0500

    tag de google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,9 +95,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="beforeInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-M598XH64');
+        `}
+      </Script>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M598XH64"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=AW-837109052"
           strategy="afterInteractive"
