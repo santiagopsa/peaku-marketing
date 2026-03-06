8171cd4c50e79e269f20cfdd269791f34d6c51d2
Santiagopsa <santiagopsa@gmail.com>
2026-03-06 11:56:43 -0500

    commit gtag guia

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,7 +96,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <Script id="gtm-script" strategy="afterInteractive">
+        <Script id="gtm-script" strategy="beforeInteractive">
           {`
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
